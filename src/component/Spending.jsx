1f016a7198167533570d857a4242f20ab6960d74
pentiumKing <ramahawai87@gmail.com>
2022-12-20 05:59:25 +0300

    Added a Method

--- src/component/Spending.jsx
+++ src/component/Spending.jsx
@@ -1,39 +1,29 @@
-import React, {useState} from 'react'
-import SpendingFilter from './SpendingFilter';
-import SpendingItem from './SpendingItem';
+import React, { useState } from "react";
+import SpendingFilter from "./SpendingFilter";
+import SpendingItem from "./SpendingItem";
 
 const Spending = (props) => {
-  const [filteredYear, setFilteredYear]=useState("2020")
+  const [filteredYear, setFilteredYear] = useState("2020");
   const filterChangeHandler = (selectedYear) => {
-    setFilteredYear(selectedYear)
-  }
+    setFilteredYear(selectedYear);
+  };
   return (
     <div>
       <div className="mt-16 bg-slate-500 max-w-5xl p-10 mx-auto rounded-xl">
-        <SpendingFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <SpendingItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
+        <SpendingFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
-        <SpendingItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <SpendingItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <SpendingItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((expense) => (
+          <SpendingItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </div>
     </div>
   );
-}
+};
 
-export default Spending
+export default Spending;
